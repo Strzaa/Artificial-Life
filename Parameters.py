a83a12cd25cd76608c535129b089feaa82ae24d7
Bartosz Fiutka <127148395+Strzaa@users.noreply.github.com>
2023-05-09 16:56:48 +0200

    Add files via upload

---
+++ Parameters.py
@@ -0,0 +1,46 @@
+# colors
+BLACK = (0, 0, 0)
+WHITE = (255, 255, 255)
+BLUE = (0, 0, 255)
+GREEN = (0, 255, 0)
+RED = (255, 0, 0)
+PURPLE = (119, 0, 200)
+
+# window
+width = 1300
+height = 700
+move = 25
+
+# parameters
+food_number = 100
+poison_number = 25
+agents_number = 50
+max_agents_number = 200
+enemy_number = 1
+
+agent_health = 1
+starting_health = 0.5
+agent_max_speed = 7
+agent_max_force = 1.5
+agent_size = 12
+
+poison_health = -0.8
+food_health = 0.4
+eatable_size = 3
+
+enemy_max_speed = 3
+enemy_max_force = 0.3
+enemy_size = 40
+
+agent_radius_min = 50
+agent_radius_max = 250
+
+circle_width = 2
+
+mutation_chance = 0.05
+health_reproduction_rate = 0.75
+
+
+
+
+
